809fca2819cc069e54f19ce7397ab1a3e895b159
revanth96 <revanthsm96@gmail.com>
2019-07-16 09:50:26 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,8 @@
-a=int(input("Enter the integer":))
-if (a%b==0):
-  print ("positive")
-elif  
-  print("Negative")
+a=int(input()
+if(a>0):
+    if(a%2)==0:
+        print("Even")
+    else:
+        print("Odd")
+else:
+    print("invalid")
